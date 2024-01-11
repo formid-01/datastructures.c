9802fd3760ae919262253ff345220adca644c501
formid <122016673+formid-01@users.noreply.github.com>
2024-01-11 21:50:06 +0300

    Create insertionSort.c

---
+++ insertionSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void insertionSort(int D[], int n){ //n sizezına sahip bir array tanımladık.
+    int i, j, key;                  //Burada atama yapabilmek için 3 tane integera ihtiyacımız var.
+    for(i = 1; i < n; i++){           
+        key = D[i];
+
+        for(j = i-1; j >= 0 && key <= D[j]; j--){
+            D[j+1] = D[j];   
+        }
+         D[j+1] = key;
+    }
+}
+
+int main(){
+
+    int n,i,j;
+   
+    printf("Please enter an array size: ");
+    scanf("%d", &n);
+
+    int D[n];
+
+      for(i = 0; i < n; i++){
+
+        printf("%d. index of array's value: ", i);
+        scanf("%d", &D[i]);
+      }
+
+      printf("\n\nAfter Insertion Sort Operation ");
+      insertionSort(D, n);
+      
+      printf("Your Array:\n");
+      for (j = 0; j < n; j++) {
+      printf("%d\t", D[j]);
+     }
+
+     return 0;
+
+}
